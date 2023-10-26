700944388714c59808d44cf643d4938e0f7f90e5
Aleksandre <grdzelishvilisandro@gmail.com>
2023-10-26 21:44:45 +0400

    first commit

---
+++ java.js
@@ -0,0 +1,6 @@
+const saburgere = document.getElementById('burger')
+const saburgere2 = document.querySelector('.burgermenu')
+
+saburgere.addEventListener('click' , function(){
+    saburgere2.classList.toggle('active')
+})
